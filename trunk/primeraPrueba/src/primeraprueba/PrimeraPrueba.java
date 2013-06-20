a8f14de3b6ff51cd38f41ad2cfc9bf27170a4f59
jcmracingrte@gmail.com <jcmracingrte@gmail.com@ca8c8bd6-3966-d07d-452d-ecc44ab752ad>
2013-06-20 16:47:35 +0000

    primera prueba subversion-netbeans-googlecode- proyecto tarea HDP

---
+++ trunk/primeraPrueba/src/primeraprueba/PrimeraPrueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeraprueba;
+
+/**
+ *
+ * @author root
+ */
+public class PrimeraPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
